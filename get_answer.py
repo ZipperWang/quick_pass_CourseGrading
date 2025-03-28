894da8a9cafa01f00a35910d318c7cb2018294dc
Zipper_Wang <2274035494@qq.com>
2025-03-28 19:11:28 +0800

    修改答案输出格式

--- get_answer.py
+++ get_answer.py
@@ -1,6 +1,5 @@
 from openai import OpenAI
 from Config import config
-from get_data import GetData
 
 
 class GetAnswer:
@@ -16,7 +15,8 @@ class GetAnswer:
             # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
             messages=[
                 {'role': 'system',
-                 'content': "现在你是一名Python初学者，你需要解决一道编程题，要求:input函数参数必须为空，严格按照题目例子输出答案，若为填空题，以列表格式输出答案，你只需要输出答案就可以."
+                 'content': "现在你是一名Python初学者，你需要解决一道编程题，要求:input"
+                            "函数参数必须为空，严格按照题目例子输出答案，若为填空题，以列表格式输出答案，你只需要输出答案就可以.并删除```python"
                             "例如['答案1','答案2']"},
                 {'role': 'user', 'content': question}],
         )
@@ -25,10 +25,10 @@ class GetAnswer:
     def get_normal_answer(self, question: str):
         completion = self.client.chat.completions.create(
             model=config['model_name'],
-            # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
             messages=[
                 {'role': 'system',
-                 'content': "现在你是一名Python初学者，你需要解决一道编程题，要求:input函数参数必须为空，严格按照题目例子输出答案，若为填空题，以列表格式输出答案，你只需要输出答案就可以."
+                 'content': "现在你是一名Python初学者，你需要解决一道编程题，要求:input"
+                            "函数参数必须为空，严格按照题目例子输出答案，若为填空题，以列表格式输出答案，你只需要输出答案就可以.并删除```python"
                             "例如: print('hello world')"},
                 {'role': 'user', 'content': question}],
         )
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
 from get_data import GetData
 from get_answer import GetAnswer
+
 # 目标 URL
 url = "https://bigdatatech.nwafu.edu.cn/assignment/index.jsp"
+
+
+def get_content(json_str):
+    import json
+    response = json.loads(json_str)
+    return response["choices"][0]["message"]["content"]
 
 
 if __name__ == "__main__":
@@ -10,5 +17,7 @@ if __name__ == "__main__":
     get_answer = GetAnswer()
     get_data.get_question_lists()
     for question in get_data.get_question():
-        print(question)
-        print(get_answer.get_normal_answer(question['题目描述']))
+        print(question["题目描述"])
+        print()
+        print(get_content(get_answer.get_normal_answer(question['题目描述'])))
+        print()
