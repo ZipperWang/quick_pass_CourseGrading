71593187fcb914e51c02ffb75de89b7fb2fe7ba6
ZipperWang <117494760+ZipperWang@users.noreply.github.com>
2025-04-10 19:34:05 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,8 +7,8 @@ config = {
         "question_list": "https://bigdatatech.nwafu.edu.cn/assignment/index.jsp"
     },
     # 用户参数设置
-    "username": "2024011843",
-    "password": ".12wzp12.",
+    "username": "your_username",
+    "password": "password",
     # 大模型参数设置
     "model_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
     "model_name": "deepseek-r1",
