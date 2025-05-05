6e05c4b9aa92aeb399ae0d502f7dbd1555d96d64
Zipper_Wang <2274035494@qq.com>
2025-05-05 18:50:27 +0800

    修复一些bug

--- Config.py
+++ Config.py
@@ -7,10 +7,10 @@ config = {
         "question_list": "https://bigdatatech.nwafu.edu.cn/assignment/index.jsp"
     },
     # 用户参数设置
-    "username": "username",
-    "password": "password",
+    "username": "2024011843",
+    "password": ".12wzp12.",
     # 大模型参数设置
     "model_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
-    "model_name": "qwen-turbo",
+    "model_name": "qwen-plus",
     "model_key": "sk-dd027f0680324f05baf269329ed97925"
 }
